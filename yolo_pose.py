dcc8db37bbc1e767c8c7afa93e5d4d00367e151d
Ghost-141 <imtiazahammed141@gmail.com>
2026-02-16 16:56:07 +0600

    handeled error

--- yolo_pose.py
+++ yolo_pose.py
@@ -4,7 +4,7 @@ import cv2
 
 model = YOLO("yolo11n-pose.pt")
 
-cap = cv2.VideoCapture("dataset/PullUps/v_Pullup_g01_c01.avi")
+cap = cv2.VideoCapture("videos/video_3.mp4")
 
 while True:
     ret, frame = cap.read()
@@ -12,10 +12,11 @@ while True:
         break
     results = model(frame)
     for result in results:
-        print(result.keypoints.xyn.cpu().numpy()[0])
+        # print(result.keypoints.xyn.cpu().numpy()[0])
         print("-------------n----------------")
-        print(len(result.keypoints.xyn.cpu().numpy()[0]))
-    cv2.imshow("Inference Window", results[0].plot())
+        # print(len(result.keypoints.xyn.cpu().numpy()[0]))
+    if result is not None:
+        cv2.imshow("Inference Window", results[0].plot())
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 cv2.destroyAllWindows()
